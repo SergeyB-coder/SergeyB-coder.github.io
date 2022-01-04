f46e8da3af3b817ea4f34a2868f6f0560359407c
татьяна леонова <senik1806@gmail.com>
2022-01-04 04:53:41 +0300

    25

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const favicon = require('express-favicon');
+//const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 8080;
 
 // здесь у нас происходит импорт пакетов и определяется порт нашего сервера
 const app = express();
-app.use(favicon(__dirname + '/build/favicon.ico')); 
+//app.use(favicon(__dirname + '/build/favicon.ico')); 
 
 //здесь наше приложение отдаёт статику
 app.use(express.static(__dirname));
